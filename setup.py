e1466149ea29d38c905b886f4f90b0c9108b118e
Jesus Hincapie <jahlon@gmail.com>
2020-05-10 21:49:37 -0500

    - se borro archivo de la carpeta designer - creados script cli.py y setup.py para la distribución de la aplicación con pyinstaller

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='editor-dibujo',
+    version='1.0.0',
+    packages=['editordibujo', 'editordibujo.ui', 'editordibujo.tests', 'editordibujo.dibujo'],
+    url='https://github.com/jahlon/editor-dibujo-repo',
+    license='MIT',
+    author='Chucho',
+    author_email='jahlon@gmail.com',
+    description='Editor de dibujo simple'
+)
